1fe2a5274ffce88ed394c392f87d145fb800d221
apemon <760108+apemon@users.noreply.github.com>
2024-04-19 16:04:32 +0700

    refactor aa project structure

--- smart-account/contracts/test/QuasarAccount.t.sol
+++ smart-account/contracts/test/QuasarAccount.t.sol
@@ -17,12 +17,12 @@ contract VotingEscrowTest is Test {
         uint256 nonce = entrypoint.getNonce(sender, 0);
 
         // Get user op fields
-        UserOperation memory op = UserOperation({
+        PackedUserOperation memory op = PackedUserOperation({
             sender: sender,
             nonce: nonce,
             initCode: _initCode,
             callData: _callDataForEntrypoint,
-            callGasLimit: 500_000,
+            accountGasLimits: 500_000,
             verificationGasLimit: 500_000,
             preVerificationGas: 500_000,
             maxFeePerGas: 0,
@@ -57,17 +57,20 @@ contract VotingEscrowTest is Test {
 
     function testAA() public {
         uint256 nonce = account.getNonce();
-        bytes transferCalldata = abi.encodeWithSignature("transfer(address,uint256)", address(account), 1000e18);
-        PackedUserOperation memory userOp = PackedUserOperation({
-            sender: address(this),
-            nonce: nonce,
-            initCode: new bytes(0),
-            callData: new bytes(0),
-            accountGasLimits: bytes32(0),
-            preVerificationGas: 21000,
-            gasFees: bytes32(0),
-            paymasterAndData: new bytes(0),
-            signature: new bytes(0)
-        });
+        // bytes transferCalldata = abi.encodeWithSignature("transfer(address,uint256)", address(account), 1000e18);
+        // PackedUserOperation memory userOp = PackedUserOperation({
+        //     sender: address(this),
+        //     nonce: nonce,
+        //     initCode: new bytes(0),
+        //     callData: new bytes(0),
+        //     accountGasLimits: bytes32(0),
+        //     preVerificationGas: 21000,
+        //     gasFees: bytes32(0),
+        //     paymasterAndData: new bytes(0),
+        //     signature: new bytes(0)
+        // });
+        // construct calldata for entrypoint
+        // create signature
+        // call entrypoint
     }
 }
